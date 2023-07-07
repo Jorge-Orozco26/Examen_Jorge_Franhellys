7ee647f3a873fde921ebe119566ed02964f6ebe8
Jorge Orozco <creneorozco@gmail.com>
2023-07-07 17:19:13 -0600

    Boton agregar y cancelar hechos

--- src/screens/homescreen.js
+++ src/screens/homescreen.js
@@ -8,25 +8,41 @@ const screenHeight = Dimensions.get("screen").height
 
 const homescreen = () => {
 
+
+    const [addNew, setAddNew] = useState(false)
      const [task, setTask] = useState('')
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style= {{marginHorizontal: 20,}}>
             {/*el comp SafeAreaView es para que el contenido de la pantalla se acople correctamente al disp */}
 
             {/*Contenedor del input */}
             <View>
                 <TextInput>
+                    onChange= {setTask}
                     placeholder = "Agregar una tarea"
                     style= {styles.input}
+                    value= {task}
                 </TextInput>
+                <View style= {{marginVertical: 10, flexDirection: 'row'}}>
+                    <TouchableOpacity
+                        style= {[styles.button, styles.acceptButton]}
+                    >
+                        <Text style= {styles.ButtonText}>Agregar</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style= {[styles.button, styles.cancelButton]}
+                    >
+                        <Text style= {styles.ButtonText}>Cancelar</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
 
 
             {/*Agregar tarea */}
             <View style = {styles.addButtonLocator}>
                 <TouchableOpacity style ={styles.addButton}>
-                    <Text style = {styles.addButtonText}></Text>
+                    <Text style = {styles.addButtonText}>+</Text>
                 </TouchableOpacity>
             </View>
 
@@ -36,6 +52,10 @@ const homescreen = () => {
 
 const styles = StyleSheet.create({
 
+
+    acceptButton:{
+        backgroundColor: 'green',
+    },
     addButton:{
         alignItems: 'center',
         backgroundColor: 950,
@@ -48,13 +68,35 @@ const styles = StyleSheet.create({
     addButtonLocator:{
         position: 'absolute',
         top: screenHeight -120,
-        right: 30
+        right: 10
     },
 
     addButtonText:{
-        color: white,
+        color: 'white',
         fontSize: 25,
     },
+
+    button:{
+        padding: 10,
+        borderRadius: 10,
+        alignSelf:'flex-start',
+    },
+
+    ButtonText:{
+        fontSize: 15,
+        color: 'white'
+    },
+
+    cancelButton: {
+        backgroundColor: 'red',
+        marginLeft: 5,
+    },
+
+    input:{
+        backgroundColor: '#dedede',
+        padding: 10,
+        borderRadius: 10, 
+    }
 })
 
 export default homescreen
