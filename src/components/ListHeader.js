ca68aee3a16052247e824cf5c969820b03adbdae
Franhellys05 <107073818+Franhellys05@users.noreply.github.com>
2023-07-08 22:11:54 -0600

    Deshabilitando linter y agregando punto y coma.

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import {View, Text } from 'react-native'
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+/* eslint-disable keyword-spacing */
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text } from 'react-native';
 
 const ListHeader = () => {
 
     return(
         <View style = {{marginBottom: 10}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold',}}>Actividades del día</Text>
+            <Text style = {{fontSize: 20, fontWeight: 'bold'}}>Actividades del día</Text>
         </View>
-    )
-}
+    );
+};
 
-export default ListHeader
+export default ListHeader;
