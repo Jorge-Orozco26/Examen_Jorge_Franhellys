7c5885a56718ae386160024abaa85638eba683e6
Franhellys05 <107073818+Franhellys05@users.noreply.github.com>
2023-07-08 22:13:55 -0600

    Formateando código y arreglando bugs

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,41 +1,38 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 
-const TaskItem = ({ onPress, task }) => {
+import React, {Component} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-    return(
-        <View style = {styles.container}>
-            <TouchableOpacity 
-                style = {styles.button} 
-                onPress={ onPress }
-            />
-            <Text style = {styles.text}>{ task }</Text>
-        </View>
-    )
-}
+const TaskItem = ({onPress, task}) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onPress} />
+      <Text style={styles.text}>{task}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'white',
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+  },
 
-    button: {
-        backgroundColor: 'white',
-        height: 30,
-        width:30,
-        borderRadius: 15,
-    },
+  container: {
+    backgroundColor: '#212121',
+    padding: 10,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 
-    container:{
-        backgroundColor: '#212121',
-        padding: 10,
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-       
-    },
+  text: {
+    color: 'white',
+    marginLeft: 10,
+  },
+});
 
-    text: {
-        color: 'white',
-        marginLeft: 10
-    },
-})
-
-export default TaskItem
+export default TaskItem;
